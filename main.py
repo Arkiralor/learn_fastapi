7837be6992bbf45f5c2102b583dec64c409bccc8
Arkiralor <prithoo11335@gmail.com>
2021-11-10 09:59:59 +0530

    none

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {'response': {'message': 'Hellow world.'}}
+
+@app.get("/blog/{id}")
+async def blog_posts(id:int):
+    return {'data': id}
+
+
+@app.get("/blog/{id}/comments")
+async def blog_comments(id:int):
+    return {'post': id, 'comments': f'comments of {id}'}
