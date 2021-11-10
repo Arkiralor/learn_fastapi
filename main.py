09e21b268d3b8070dad2be7abce6e0a8888b537f
Arkiralor <pritho11335@gmail.com>
2021-11-10 14:01:15 +0530

    none

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -7,11 +9,27 @@ app = FastAPI()
 async def index():
     return {'response': {'message': 'Hellow world.'}}
 
+
+@app.get("/blog")
+async def blogs(limit = 10, published:bool = True, sort: Optional[str] = None ):
+    if published:
+        return {'data': f'{limit} published blogs.'}
+    else:
+        return {'data': f'List of {limit} blogs.'} 
+    
+
+
+
+@app.get("/blog/unpublished")
+async def unpublished_blogs():
+    return {'data': 'Unpublished Blogs'}
+
 @app.get("/blog/{id}")
 async def blog_posts(id:int):
     return {'data': id}
 
 
 @app.get("/blog/{id}/comments")
-async def blog_comments(id:int):
-    return {'post': id, 'comments': f'comments of {id}'}
+async def blog_comments(id:int, limit:int =50):
+
+    return {'post': id, f'{limit} comments': f'{limit} comments of post: {id}.'}
