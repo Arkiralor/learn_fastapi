9024fd3acf6c737543de70a0000f0bfcbf985077
Arkiralor <pritho11335@gmail.com>
2021-11-11 15:35:29 +0530

    branches

--- blog/main.py
+++ blog/main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
 app = FastAPI()
 
 @app.post("/blog")
-async def create():
-    return {'response': 'Blog post created.'}
+async def create(request: Blog):
+    return {'response': f'Blog post {request.title} created.'}
