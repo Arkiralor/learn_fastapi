f62f863eabb77930eb9eceffe4d6e57d2f19ad8e
Arkiralor <pritho11335@gmail.com>
2021-11-23 13:46:57 +0530

    Added uauth in post delete

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -36,12 +36,23 @@ async def create(request: schemas.Blog, db: Session = Depends(get_db),
 
 # Delete blog post in DB via Post_ID
 @router.delete("/delete/{pid:int}", status_code=status.HTTP_204_NO_CONTENT)
-async def destroy(pid, db: Session = Depends(get_db)):
+async def destroy(pid, db: Session = Depends(get_db), 
+                current_user: schemas.ShowUser = Depends(get_current_user)
+                ):
     try:
-        db.query(models.Blog).filter(models.Blog.post_id ==
-                                     pid).delete(synchronize_session=False)
-        db.commit()
-        return {'response': f'Blog post with id= {pid} was successfully deleted.'}
+        current_active_user: schemas.ShowUser = db.query(models.User).filter(
+                                                    models.User.user_email == current_user.user_email).first()
+
+        post_to_be_deleted = db.query(models.Blog).filter(models.Blog.post_id == pid).first()
+
+        if post_to_be_deleted.author_id == current_active_user.user_id:
+            db.query(models.Blog).filter(models.Blog.post_id ==
+                                        pid).delete(synchronize_session=False)
+            db.commit()
+            return {'response': f'Blog post with id= {pid} was successfully deleted.'}
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail=f'Bad Request: Credentials Unauthorised.')
 
     except Exception as e:
         return {'Error': f'{e}. Blog post with id= {pid} does not exist.'}
